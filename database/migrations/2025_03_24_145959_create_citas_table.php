83c1b63b21eba90bce8a29983a906f5cd8a8ebe4
Luis-RipSar <luiripsar@alu.edu.gva.es>
2025-03-25 20:33:51 +0100

    feat: Comenzado proyecto Coraymente

---
+++ database/migrations/2025_03_24_145959_create_citas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('citas', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('id_usuario');
+            $table->uuid('id_profesional');
+            $table->timestamp('fecha');
+            $table->string('motivo');
+            $table->string('estado');
+            $table->foreign('id_usuario')->references('id')->on('users');
+            $table->foreign('id_profesional')->references('id')->on('profesional');
+            $table->unique(['id_profesional', 'fecha']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('citas');
+    }
+};
