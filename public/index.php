44d4129abc2048f8d5084469fad2dd43f35e1ce5
Luis-RipSar <luiripsar@alu.edu.gva.es>
2025-04-04 16:55:16 +0000

    Añadiendo proyecto en servidor

--- public/index.php
+++ public/index.php
@@ -18,3 +18,17 @@ require __DIR__.'/../vendor/autoload.php';
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
 $app->handleRequest(Request::capture());
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>hora    </h1>
+</body>
+</html>
